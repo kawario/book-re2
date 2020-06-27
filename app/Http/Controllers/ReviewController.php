5c812b70dcd63897761d220b5190b4d8d42d1db2
EC2 Default User <ec2-user@ip-172-31-29-184.us-east-2.compute.internal>
2020-06-27 01:38:47 +0000

    first commit

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// ==========ここから追加する==========
+use App\Review;
+// ==========ここまで追加する==========
+
+class ReviewController extends Controller
+{
+    //ここから追加する
+    public function index()
+    {
+        $reviews = Review::where('status', 1)->orderBy('created_at', 'DESC')->paginate(3);
+    	 return view('index', compact('reviews'));
+    }
+    
+    // ==========ここから追加する==========
+public function show($id)
+{
+    $review = Review::where('id', $id)->where('status', 1)->first();
+
+    return view('show', compact('review'));
+}
+// ==========ここまで追加する==========
+    
+     public function create()
+    {
+        return view('review');
+    }
+    
+    public function store(Request $request)
+    {
+        $post = $request->all();
+    
+    // ==========ここから追加する==========
+    $validatedData = $request->validate([
+        'title' => 'required|max:255',
+        'body' => 'required',
+        'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ]);
+// ==========ここまで追加する==========
+        
+    if ($request->hasFile('image')) {
+        
+     $request->file('image')->store('/public/images');
+      $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];        
+    
+    } else {
+          $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+      }
+      
+    Review::insert($data);
+
+        return redirect('/')->with('flash_message', '投稿が完了しました');
+    }
+}
